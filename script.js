9f2cdb7f05b48abe6cf208bdf227c10f2152a192
Developer Aqeel <aqeel471860438@gmail.com>
2025-12-25 19:13:34 -0800

    Working on !

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let userName = document.querySelector("#inp1").value
-let userRollNum = document.querySelector("#inp2").value
 let form = document.querySelector('.login')
 let wcScreen = document.querySelector('.wc-con')
+let wcH1 = document.querySelector('.wc-con h1')
+wcScreen.style.display= 'none';
+
 function formSubmit(){
+    let userName = document.querySelector("#inp1").value
+    let userRollNum = document.querySelector("#inp2").value
  const userData = {
     userName,
     userRollNum
@@ -11,5 +14,7 @@ function formSubmit(){
 //  console.log(userData.userName)
  form.style.display= 'none';
  wcScreen.style.display= 'flex';
+
+ wcH1.innerHTML=`Welcome ${userName}`
 
 }
